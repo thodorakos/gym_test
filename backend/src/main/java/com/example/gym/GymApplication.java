ee05674fdac3784db06d2aa4466838970b4cc937
thodorakos <csd4965@uoc.csd.gr>
2025-10-17 18:38:11 +0300

    chnaged main

--- backend/src/main/java/com/example/gym/GymApplication.java
+++ backend/src/main/java/com/example/gym/GymApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GymApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GymApplication.class, args);
-    }
-
-}
+	public static void main(String[] args) {
+		System.out.println("--- Verifying Environment Variables before Spring starts ---");
+		System.out.println("SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+		System.out.println("SERVER_PORT: " + System.getenv("SERVER_PORT"));
+		System.out.println("----------------------------------------------------");
+		SpringApplication.run(GymApplication.class, args);
+	}}
